1de8d4020e73244f69514274cdb7244323116340
Ines <ines.salhi@student.ecp.fr>
2014-04-21 22:20:33 -0400

    test

---
+++ testapp copia/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testapp::Application.config.secret_key_base = '24606b3515f93aeabf518df9476a96c540c156a186979782c7c806004bda9bd593d19cf5bfc1bcbe7f3d54c4ba3185d67e467b1438a1caeeba3120d581602a03'
